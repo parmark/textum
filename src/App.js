fdc165fe5dd311bda457afe9d1b2e1da8c2ea294
Kevin Parmar <parmarkevinb@gmail.com>
2020-04-30 01:02:26 -0400

    Edited App to use components and provide functional posting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,102 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Thread from './components/Thread';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  state = {
+    threads: [
+      {
+        id: 0,
+        posts: [
+          {
+            name: 'Nonomy', 
+            content: '^&%*&^$%*&'
+          }, 
+          {
+            name: 'Chii-sama', 
+            content: '&*^#*(&*($....?'
+          }, 
+          {
+            name: null, 
+            content: '....!!'
+          }
+        ] 
+      }, 
+      {
+        id: 1, 
+        posts: [
+          {
+            name: 'Nonomy', 
+            content: '^&%*&^$%*&'
+          }, 
+          {
+            name: 'Chii-sama', 
+            content: "&*^#*(&*($....?"
+          }, 
+          {
+            name: null, 
+            content: '....!!'
+          }
+        ] 
+      }
+    ]
+  }
+
+  // Handles replies to existing threads
+  handleReply = (props) => {
+    let threads = [...this.state.threads];
+
+    threads[props.id].posts.push(
+      {name: document.getElementById(`name-field-${props.id}`).value, 
+      content: document.getElementById(`post-field-${props.id}`).value}
+    )
+
+    console.log(this.state)
+
+    this.setState(() => threads)
+
+    console.log(this.state)
+  }
+
+  // Handles replies to new threads
+  handleCreate = (props) => {
+    let threads = [...this.state.threads];
+
+    threads.push({
+      id: this.state.threads.length,
+      posts: [
+        {
+          name:  document.getElementById(`name-field-${props.id}`).value,
+          content: document.getElementById(`post-field-${props.id}`).value
+        }
+      ] 
+    })
+
+    this.setState(() => ({threads}))
+
+  }
+
+  render() {
+    return (
+        <div className='App'>
+            {this.state.threads.map(thread => 
+              <Thread 
+                thread={thread}
+                reply={this.handleReply}
+                key={thread.id}
+              />
+            )}
+            <Thread 
+              thread={
+                {
+                  id: this.state.threads.length,
+                  posts: []
+                }
+              }
+              reply={this.handleCreate}
+            />
+        </div>
+    );
+  }
 }
 
 export default App;
