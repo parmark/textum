6f0ac734a12ef72257b41d8002a0383475e7ad17
Kevin Parmar <parmarkevinb@gmail.com>
2020-05-05 16:38:51 -0400

    Added post ids

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -4,9 +4,9 @@ function Post(props) {
     return (
         <div className='post'>
             {props.name ? 
-                <p className='name'>{props.name}</p>
+                <p className='name'>{props.name} No. {props.id}</p>
             :
-                <p className='name'>Anonymous</p>
+            <p className='name'>Anonymous No. {props.id}</p>
             }
             <p className='content'>{props.content}</p>
 
