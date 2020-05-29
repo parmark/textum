ac3deb7cd9630f844eb52c925f333a443738af54
Kevin Parmar <parmarkevinb@gmail.com>
2020-05-28 22:45:17 -0400

    Edited reply box styling

--- src/components/ReplyBox/index.jsx
+++ src/components/ReplyBox/index.jsx
@@ -10,14 +10,11 @@ function ReplyBox(props) {
     return (
             <form>
                 {/* Name field */}
-                <span className="input">
+                <span className="name-input">
                     <input className="input__field" type="text" id={`name-field-${props.index}`} name='text' placeholder="Name"/>
-                    {/* <label htmlFor={`name-field-${props.index}`} className="input__label">
-                        <span className="input__label-content"></span>
-                    </label> */}
                 </span>
                 {/* Post field */}
-                <span>
+                <span className="content-input">
                     <input type="text" id={`post-field-${props.index}`} placeholder="Post"/>
                 </span>
                 <button className="submit" onClick={handleReply}>Reply</button>
